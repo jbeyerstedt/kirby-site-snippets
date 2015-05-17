74042eb78aced5e6d21a0ab12f144432aa6aac28
Jannik Beyerstedt <code@jannikbeyerstedt.de>
2015-05-17 14:19:52 +0200

    plg-navbar v1.2.1 - option to hide menu_title

--- plg-navbar.php
+++ plg-navbar.php
@@ -11,16 +11,22 @@
 // snippet('plg-navbar'); 
 // in the config.php:
 // c::set('navbar-class', 'navbar-default');
+// c::set('navbar-affix', true); //needs some boostrap script!
+// c::set('navbar-ignoreChildren', true); //disable dropdowns for children pages
 
-// version: 1.2.0 (20.02.2015)
+// version: 1.2.1 (14.05.2015)
 // changelog: 
 // v1.1.0: add config option for the navbar style class
 // v1.2.0: move .container in nav.navbar for affix scripts
+// v1.2.1: make menu_title optional (remove button if text is empty)
 // -------------------------------------------
 
 // add default navbar-class if not set in config.php
 $class = c::get('navbar-class');
 if(!isset($class)) $class = 'navbar-inverse';
+
+$ignoreChildren = c::get('navbar-ignoreChildren');
+if(!isset($ignoreChildren)) $ignoreChildren = false;
 ?>
 
 <div class="navbar-wrapper">
@@ -33,17 +39,19 @@ if(!isset($class)) $class = 'navbar-inverse';
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="<?php echo url() ?>"><?php echo html($site->menu_title())?></a>
+<?php if( !empty((string)$site->menu_title()->html()) ) : ?>
+    <a class="navbar-brand" href="<?php echo url() ?>"><?php echo $site->menu_title()->html()?></a>
+<?php endif; ?>
   </div>
 
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav">        
 <?php foreach($pages->visible() AS $p): ?>
-<?php $subs = ($p->hasChildren()) ? $p->children() : false ?>
+<?php $subs = ($p->hasVisibleChildren()) ? $p->children() : false ?>
 
-<?php if ($subs && $subs->count()): ?>
-      <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo html($p->title()) ?> <span class="caret"></span></a>
+<?php if ($subs && $subs->count() && !$ignoreChildren): ?>
+      <li class="dropdown <?php echo ($p->isOpen()) ? 'active' : '' ?>"> <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo html($p->title()) ?> <span class="caret"></span></a>
         <ul class="dropdown-menu" role="menu">
 <?php foreach($subs->visible() AS $s): ?>
           <li><a href="<?php echo $s->url() ?>"> <?php echo $s->title()?> </a></li>
